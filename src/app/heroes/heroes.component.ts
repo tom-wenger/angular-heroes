b0b05ab3733c0e016005b601f8b8ff2771557eb8
Tom Wenger <thomiwenger@gmail.com>
2022-11-22 17:09:34 +0100

    End of original Tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,11 +18,7 @@ import { MessagesService } from '../messages.service';
 
 const arr = [1, 2, 3];
 const n = arr.reduce((agg, v) => agg + v, 0);
-//  const m = arr.reduce((agg, v) => [...agg, v], [] as number[]);
-
-// 1
-// 3
-// 6
+const m = arr.reduce((agg, v) => [...agg, v], [] as number[]);
 
 @Component({
   selector: 'app-heroes',
@@ -32,55 +28,24 @@ const n = arr.reduce((agg, v) => agg + v, 0);
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  public heroes$!: Observable<Hero[]>;
-  // public newHero$ = new Subject<Hero>();
-  public addHero$ = new Subject<string>();
-
   constructor(
     private heroService: HeroService,
     private messageService: MessagesService
   ) {}
 
   getHeroes(): void {
-    // this.heroes$ = merge(this.heroService.getHeroes(), this.newHeros$);
-
-    //mergemap
-    const fromServerStream$ = this.heroService.getHeroes();
-    const addedHero$ = this.addHero$.pipe(
-      map((name) => name.trim()),
-      filter((name) => Boolean(name)),
-      concatMap((name) => this.heroService.addHero(name))
-    );
-
-    this.heroes$ = fromServerStream$.pipe(
-      mergeMap((a) =>
-        addedHero$.pipe(
-          scan((agg, v) => [...agg, v], a),
-          startWith(a)
-        )
-      )
-    );
-
-    // this.heroes$ = fromServerStream$.pipe(
-    //   mergeMap((heroes) => this.newHero$.pipe(map((hero) => [...heroes, hero])))
-    // );
-
-    //alt
-    // this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
   add(name: string): void {
-    this.addHero$.next(name);
-    // name = name.trim();
-    // if (!name) {
-    //   return;
-    // }
-    // this.heroService.addHero({ name } as Hero).subscribe((hero) => {
-    //   // console.log(hero);
-    //   // this.heroes.push(hero);
-    //   this.newHero$.next(hero);
-    //   // this.getHeroes();
-    // });
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      console.log(hero);
+      this.heroes.push(hero);
+    });
   }
 
   delete(hero: Hero): void {
